52b72ffdbb1ba9a6ba27ae16825607d2d7b4eb69
davidco@hotmail.hu <Hesoyam333>
2018-03-07 19:58:40 +0100

    Upgraded recursive Flatmap

--- src/main/scala/lists/P07/FlattenList.scala
+++ src/main/scala/lists/P07/FlattenList.scala
@@ -8,4 +8,23 @@ object FlattenList {
       case l: List[_] => flattenWithBuiltIn(l)
       case e => List(e)
     }
+
+  def flattenWithTailRecursive(list: List[Any]): List[Any] = {
+    def matchCases(element: Any): List[Any] = element match {
+      case l: List[_] => flattenWithTailRecursive(l)
+      case e => List(e)
+    }
+
+    @annotation.tailrec
+    def helper(r: List[Any], ls: List[Any]): List[Any] = {
+      ls match {
+        case Nil => r
+        case head :: tail => helper(r ::: matchCases(head), tail)
+      }
+    }
+
+    helper(Nil, list)
+  }
+
+
 }
--- src/test/scala/lists/P07/FlattenListTest.scala
+++ src/test/scala/lists/P07/FlattenListTest.scala
@@ -13,6 +13,11 @@ class FlattenListTest extends FunSuite {
     testWithNonEmptyList(FlattenList.flattenWithBuiltIn)
   }
 
+  test("testFlattenWithTailRecursive") {
+    testWithEmptyList(FlattenList.flattenWithTailRecursive)
+    testWithNonEmptyList(FlattenList.flattenWithTailRecursive)
+  }
+
   def testWithEmptyList(functionToTest: List[Any] => List[Any]): Unit = {
     val result = functionToTest(Nil)
 
@@ -20,8 +25,8 @@ class FlattenListTest extends FunSuite {
   }
 
   def testWithNonEmptyList(functionToTest: List[Any] => List[Any]): Any = {
-    val testList = List(1, 2, List(3, 4), List(5, 6), 7, 8)
-    val expectedList = List(1, 2, 3, 4, 5, 6, 7, 8)
+    val testList = List(1, List(3, 4,List(5, 6, List(7, 8, 9))))
+    val expectedList = List(1, 3, 4, 5, 6, 7, 8, 9)
 
     val result = ExecutionTimeUtils time functionToTest(testList)
 
